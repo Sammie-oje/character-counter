9b7155327f89e6a0a8db31b853f9a24f05ed31cf
Sammie-oje <omokhojesamuel8@gmail.com>
2026-03-27 03:00:01 +0100

    I was always great at cooking, added a counter animation to the metric values when they are in view

--- src/components/metrics/Stat.jsx
+++ src/components/metrics/Stat.jsx
@@ -2,15 +2,26 @@ import { animate, motion, useMotionValue, useTransform } from "motion/react";
 import { useEffect } from "react";
 
 function Stat({ bgColor, id, text, onCount }) {
+    const count = useMotionValue(0);
+    const rounded = useTransform(() =>
+        Math.round(count.get()).toLocaleString()
+    );
+
     return (
         <div
             className={`p-250 ${bgColor} rounded-12 overflow-hidden relative h-[130px] w-full md:h-full md:px-150 md:py-200 xxl:p-200`}
             id={id}
         >
             <div className="flex flex-col gap-100 h-full justify-center">
-                <dt className="text-preset-mobile md:text-preset-1">
-                    {onCount ? onCount : "00"}
-                </dt>
+                <motion.dt
+                    className="text-preset-mobile md:text-preset-1"
+                    onViewportEnter={() => {
+                        animate(count, onCount, { duration: 2 });
+                    }}
+                    viewport={{ once: true }}
+                >
+                    {rounded}
+                </motion.dt>
                 <dd className="text-preset-3 z-100">{text}</dd>
             </div>
             <figure className="absolute -top-2 -right-15 ">
